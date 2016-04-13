edf54aefd6fee2f6e32f8c3b0b355c176110925f
nimstill <1679917335@qq.com>
2016-04-13 21:56:29 +0800

    shuzu

--- first.js
+++ first.js
@@ -7,3 +7,15 @@ function factorial(number) {
     }
 }
 print(factorial(5));
+
+
+//编写出容易阅读的代码和编写出让计算机能正确执行的代码同等重要
+
+
+//shenfuzhi
+function copy(arr1, arr2) {
+    for (var i = 0; i<arr.length; ++i){
+        arr2[i] = arr1[i];
+    }
+}
+
